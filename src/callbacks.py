9bf0742cbafe7d70afb168cbfac0a33692bd500d
kagev <kaplitskiygv@gmail.com>
2025-01-05 19:44:01 +0200

    fix trouble

--- src/callbacks.py
+++ src/callbacks.py
@@ -270,7 +270,7 @@ def register_callbacks(app):
     )
     def update_model_chart(n_clicks):
         # Завантаження даних з JSON
-        file_path = os.path.join(get_root_path(), 'src/results', 'model_evaluation.json')
+        file_path = os.path.join(get_root_path(), 'results', 'model_evaluation.json')
 
 
         with open(file_path) as f:
